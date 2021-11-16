a0af411183b64cc474b36d84d57aa0f7502e958a
Siarhei Shabalin <SShabalin@alfabank.ru>
2021-11-16 12:57:31 +0300

    added lombok

--- src/test/java/ru/shabalin/utils/StepData.java
+++ src/test/java/ru/shabalin/utils/StepData.java
@@ -1,26 +1,12 @@
 package ru.shabalin.utils;
 
 import io.restassured.response.Response;
+import lombok.Data;
 
 import java.util.List;
 
+@Data
 public class StepData {
     private Response response;
     private List<List<String>> responseList;
-
-    public Response getResponse() {
-        return response;
-    }
-
-    public void setResponse(Response response) {
-        this.response = response;
-    }
-
-    public List<List<String>> getResponseList() {
-        return responseList;
-    }
-
-    public void setResponseList(List<List<String>> responseList) {
-        this.responseList = responseList;
-    }
 }
